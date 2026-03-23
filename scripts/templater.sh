b648a1a02639f3a00aaca0c280ccaeaedd9dbfb5
rayyanshahid-dev <rayyanshahid.dev@proton.me>
2026-03-23 12:38:24 +0800

    updated template script

--- scripts/templater.sh
+++ scripts/templater.sh
@@ -7,3 +7,50 @@ cd "$dir_name" &&
 
 mkdir includes/ src/  &&
 touch Makefile README.md
+
+# gitignore
+
+cat > .gitignore << EOF
+
+*.o
+*.a
+*.so
+*.exe
+*.out
+a.out
+
+EOF
+
+# Makefile
+
+cat > Makefile << EOF
+
+SRCDIR = src/
+SRCS   = src/*.c
+CC 	   = gcc
+TARGET = smartroutes 
+
+CFLAGS = -Wall -Wextra -O3 
+LDFLAGS = -lX11 -lm
+
+# Default target
+all: $(TARGET)
+
+# Rule to build the target executable
+$(TARGET): $(SRCS)
+	$(CC) $(CFLAGS) -o $(TARGET) $(SRCS) $(LDFLAGS)
+
+run:
+	./$(TARGET)
+
+# Clean target to remove the compiled binary
+clean:
+	rm -f $(TARGET)
+
+EOF
+
+git init
+git add .
+git commit -m "first commit"
+
+echo "project "$dir_name" created and initialized"
